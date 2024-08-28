fcc6f9c8f9799eb620e07808f0a4a7020683e850
Muhammad Ahmed Siddiqui <154745545+mahmedsiddiqui@users.noreply.github.com>
2024-08-28 17:25:40 +0500

    Add files via upload

---
+++ Java script (Revision)/app.js
@@ -0,0 +1,49 @@
+//first player
+
+function inc() {
+  
+
+    let first = document.getElementById("one")
+
+
+    first.innerText = parseInt(first.innerText) + 1
+
+}
+
+function dec() {
+    
+    
+    let sec = document.getElementById("one")
+
+
+    sec.innerText = parseInt(sec.innerText) - 1
+    
+    
+
+
+}
+
+//second player
+
+
+
+function second_inc() {
+    let first = document.getElementById("two")
+
+
+    first.innerText = parseInt(first.innerText) + 1
+
+}
+
+function second() {
+
+
+
+
+    let sec = document.getElementById("two")
+
+
+    sec.innerText = parseInt(sec.innerText) - 1
+
+
+}
